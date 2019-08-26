5398b589c6854b351e7818818c4a55daee5d6a97
aashish27 <aashish.yadav@dhwaniris.com>
2019-08-26 22:18:55 +0530

    Updates

---
+++ precache-manifest.326d3834befbcc9aadff366966ea4a1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25d0cc0d23d6420395cbfbcf72f89da",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0ab86d3c232704a8a2bd",
+    "url": "/monsters-rolodex/static/css/main.7366fde6.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "0ab86d3c232704a8a2bd",
+    "url": "/monsters-rolodex/static/js/main.88629c31.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
